89f6fd31793c7e16f23d06d9b2ec15017132d227
mariereine22 <mariereinebigabwa@gmail.com>
2023-08-17 15:57:01 +0200

    Ajout de l'API get ville

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
+from getweather import meteo,previsions
 
 print("hello everyone")
+api_key = "1fceafa4d5469574428c00f485b0b738"
+app = FastAPI()
+
+@app.get("/")
+def read_route():
+    return {"hello":"marie"}
+
+@app.get("/meteo/{ville}")
+def envoidata(ville:str):
+    return (meteo(ville,api_key))
+
+@app.get("/previsions/{ville}")
+def envoidata(ville:str):
+    return(previsions(ville,api_key))
+
+
+
+    
--- getweather.py
+++ getweather.py
@@ -6,13 +6,13 @@ api_key = "1fceafa4d5469574428c00f485b0b738"
 
 def meteo(city_name,api_key):
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&lang=fr"
     response = requests.get(complete_url)
     data = response.json()
     return data
 def previsions(city_name,api_key):
     base_url = "http://api.openweathermap.org/data/2.5/forecast?"
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&lang=fr"
     response = requests.get(complete_url)
     data = response.json()
     return data
